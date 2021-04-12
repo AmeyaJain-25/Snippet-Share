8560856fece767ba839143239b6da2845ff24a2e
AmeyaJain-25 <ameiya.jain@hotmail.com>
2021-04-12 10:59:28 +0530

    Routing Added

---
+++ snippet-share/src/Routes.js
@@ -0,0 +1,40 @@
+import React from "react";
+//Packages-----------------
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+//Components-----------------
+import Signin from "./user/Signin/Signin";
+import Signup from "./user/Signup/Signup";
+import PrivateRoutes from "./auth/helper/PrivateRoutes";
+import Home from "./core/Home";
+import CreatePost from "./core/CreatePost";
+import UpdatePost from "./core/UpdatePost";
+import Profile from "./core/Profile";
+import OtherProfile from "./core/OtherProfile";
+import ViewPost from "./core/ViewPost";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Signin} />
+        <Route path="/signup" exact component={Signup} />
+        <PrivateRoutes path="/home" exact component={Home} />
+        <PrivateRoutes path="/post/create" exact component={CreatePost} />
+        <PrivateRoutes path="/post/view/:postId" exact component={ViewPost} />
+        <PrivateRoutes path="/profile" exact component={Profile} />
+        <PrivateRoutes
+          path="/otherProfile/:userid"
+          exact
+          component={OtherProfile}
+        />
+        <PrivateRoutes
+          path="/post/update/:postId"
+          exact
+          component={UpdatePost}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
--- snippet-share/src/index.js
+++ snippet-share/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Routes from "./Routes"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
