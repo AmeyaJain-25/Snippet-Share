fc44c609b41a6206eaf4db4a9868329e5764c499
AmeyaJain-25 <ameiya.jain@hotmail.com>
2021-04-12 10:54:13 +0530

    Signin Added

---
+++ snippet-share/src/user/Signin/Signin.js
@@ -0,0 +1,190 @@
+import React, { useState } from "react";
+import "./signin.css";
+//Packages-----------------
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Link, Redirect } from "react-router-dom";
+//Function importing-----------------
+import { signin, authenticate, isAuthenticated } from "../../auth/helper/index";
+//Components-----------------
+import LoginInput from "../../components/LoginInput";
+//Images-----------------
+import logoWithSlogan from "../../logoWithSlogan.png"
+
+const Signin = () => {
+  //useState---------------
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    username: "",
+    loading: false,
+    didRedirect: false,
+  });
+  const [disableSignin, setDisableSignin] = useState(false);
+
+  const { email, password, username, error, loading, didRedirect } = values;
+
+  const { user } = isAuthenticated();
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  //Signin Function---------------
+  const onSubmit = (event) => {
+    setDisableSignin(true);
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    //API call---------------
+    signin({ email, password, username })
+      .then((data) => {
+        setDisableSignin(false);
+        if (data.error) {
+          setValues({
+            ...values,
+            error: data.error,
+            loading: false,
+            didRedirect: false,
+          });
+        } else {
+          authenticate(data, () => {
+            setValues({
+              ...values,
+              didRedirect: true,
+              error: false,
+              loading: false,
+              email: "",
+              username: "",
+              error: "",
+            });
+          });
+        }
+      })
+      .catch(console.log("Sign In Failed"));
+  };
+
+  //Perform Redirect---------------
+  const performRedirect = () => {
+    if (didRedirect) {
+      if (user && user.role === 1) {
+        return <Redirect to="/admin/dashboard" />;
+      } else {
+        return <Redirect to="/home" />;
+      }
+    }
+    if (isAuthenticated()) {
+      return <Redirect to="/home" />;
+    }
+  };
+
+  //LOADING Message---------------
+  const loadingMessage = () => {
+    return (
+      loading && (
+        <h2
+          style={{
+            display: loading ? "" : "none",
+            fontSize: "1em",
+            color: "#00ab66",
+            textAlign: "center",
+          }}
+        >
+          Loading......!
+        </h2>
+      )
+    );
+  };
+
+  //ERROR Message---------------
+  const errorMessage = () => {
+    return (
+      <h3
+        style={{
+          display: error ? "" : "none",
+          fontSize: "1em",
+          color: "red",
+          textAlign: "center",
+        }}
+      >
+        {error}
+      </h3>
+    );
+  };
+
+  //Signin Page JSX---------------
+  const signInForm = () => {
+    return (
+      <Container className="themed-container signin_container" fluid>
+        <Row>
+          <Col md="7" sm="12">
+            <Container className="signin_left_container">
+              <Row>
+                <img
+                  src={logoWithSlogan}
+                  alt="dostiKattaLogo"
+                  className="dostiKatta_name"
+                />
+              </Row>
+              <Row className="below_logo">
+                <h4>
+                  Meet millions of new people from all over the world and connect with them to make new friends.
+                </h4>
+              </Row>
+            </Container>
+          </Col>
+          <Col md="5" sm="12">
+            <Container className="signin_right_container">
+              <Row className="signup_note_div">
+                <h3 className="signup_note">
+                  Not a Member?{" "}
+                  <Link to="/signup" style={{ textDecoration: "none" }}>
+                    <span>Sign UP</span>
+                  </Link>
+                </h3>
+              </Row>
+              <Row>
+                <div className="signin_signin_box">
+                  <LoginInput
+                    labelName="Email"
+                    inputType="email"
+                    value={email}
+                    onChangeEvent={handleChange("email")}
+                  />
+                  <h5 style={{ color: "black", textAlign: "center" }}>Or</h5>
+                  <LoginInput
+                    labelName="Username"
+                    inputType="username"
+                    value={username}
+                    onChangeEvent={handleChange("username")}
+                  />
+                  <LoginInput
+                    labelName="Password"
+                    inputType="password"
+                    value={password}
+                    onChangeEvent={handleChange("password")}
+                  />
+                  {loadingMessage()}
+                  {errorMessage()}
+                  <Button onClick={onSubmit} className="signin_button" disabled={disableSignin}>
+                    SIGN IN
+                  </Button>
+                </div>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
+
+  return (
+    <>
+      {signInForm()}
+      {performRedirect()}
+    </>
+  );
+};
+
+export default Signin;
+
+//Perform Redirect
---
+++ snippet-share/src/user/Signup/Signup.js
@@ -0,0 +1,168 @@
+import React, { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Link, Redirect } from "react-router-dom";
+import { signup } from "../../auth/helper/index";
+import LoginInput from "../../components/LoginInput";
+import logoWithSlogan from "../../logoWithSlogan.png";
+import "./signup.css";
+
+const Signup = () => {
+  //useState---------------
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    username: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+  const [disableSignup, setDisableSignup] = useState(false);
+
+  const { name, email, username, password, error, success } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  //Signup Function---------------
+  const onSubmit = (event) => {
+    setDisableSignup(true);
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    //API call---------------
+    signup({ name, email, username, password })
+      .then((data) => {
+        setDisableSignup(false);
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            username: "",
+            error: "",
+            password: "",
+            success: true,
+          });
+        }
+      })
+      .catch(console.log("Error in SignUp"));
+  };
+
+  //SUCCESS Message---------------
+  const successMessage = () => {
+    return (
+      <div>
+        <h3
+          style={{
+            display: success ? "" : "none",
+            fontSize: "1em",
+            color: "#00ab66",
+            textAlign: "center",
+          }}
+        >
+          New Account Created Successfully
+          <br />
+          <Link to="/"> Please Login Here</Link>
+        </h3>
+        {success && <Redirect to="/" />}
+      </div>
+    );
+  };
+
+  //ERROR Message---------------
+  const errorMessage = () => {
+    return (
+      <h3
+        style={{
+          display: error ? "" : "none",
+          fontSize: "1em",
+          color: "red",
+          textAlign: "center",
+        }}
+      >
+        {error}
+      </h3>
+    );
+  };
+
+  //Signup Page JSX---------------
+  const signUpForm = () => {
+    return (
+      <Container className="themed-container signup_container" fluid>
+        <Row>
+          <Col md="6" sm="12">
+            <Container className="left_container">
+              <div className="signup_box">
+                <p className="register_text">Register</p>
+                <hr />
+                <LoginInput
+                  labelName="Name"
+                  inputType="text"
+                  value={name}
+                  onChangeEvent={handleChange("name")}
+                />
+                <LoginInput
+                  labelName="Email"
+                  inputType="email"
+                  value={email}
+                  onChangeEvent={handleChange("email")}
+                />
+                <LoginInput
+                  labelName="Username"
+                  inputType="text"
+                  value={username}
+                  onChangeEvent={handleChange("username")}
+                />
+                <LoginInput
+                  labelName="Password"
+                  inputType="password"
+                  value={password}
+                  onChangeEvent={handleChange("password")}
+                />
+                <p className="strong_password_note">
+                  Use 5 or more characters to make a strong Password.
+                </p>
+                {successMessage()}
+                {errorMessage()}
+                <Button onClick={onSubmit} className="register_button" disabled={disableSignup}>
+                  REGISTER
+                </Button>
+              </div>
+            </Container>
+          </Col>
+          <Col md="6" sm="12">
+            <Container className="right_container">
+              <Row className="dostiKatta_name_row">
+                <img
+                  src={logoWithSlogan}
+                  alt="dostiKattaLogo"
+                  className="dostiKatta_name"
+                />
+              </Row>
+              <Row className="below_logo">
+                <h4>
+                  {/* Meet millions of new people from all over the world, wherever
+                  you are. Have a good chat, make new friend. */}
+                </h4>
+              </Row>
+              <Row className="signup_note_div">
+                <h3 className="signin_note">
+                  Already a Member?{" "}
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    <span>Sign IN</span>
+                  </Link>
+                </h3>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
+
+  return <>{signUpForm()}</>;
+};
+
+export default Signup;
